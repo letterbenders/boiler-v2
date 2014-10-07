dcad1472810aa050cf6dc139b0b432ce1868e170
asger@letterbenders.com <asger@letterbenders.com>
2014-10-08 00:18:42 +0200

    init commit

---
+++ config.rb
@@ -0,0 +1,6 @@
+# Compass
+
+css_dir = 'build/css'
+sass_dir = 'src/sass'
+javascript_dir = 'build/js'
+output_style = :compressed
---
+++ gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			options: {
+		      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+		        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+		    },
+		    my_target: {
+		      files: {
+		        'build/js/output.min.js': ['src/js/*.js']
+		      }
+		    }
+		},
+		compass: {
+			dev: {
+				options: {
+					config: 'config.rb'
+				} //options
+			} //dev
+		}, // compass
+		watch: {
+			options: {
+			    livereload: true,
+    		},
+    		scripts: {
+				tasks: ['uglify']
+			}, //scripts
+			html: {
+				files: ['*.html']
+			}, // html
+			sass: {
+				files: ['src/sass/*.scss'],
+				tasks: ['compass:dev']
+			} //sass
+		} //watch
+	}) //initConfig
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+
+	grunt.registerTask('default', ['watch']);
+
+
+} //exports
