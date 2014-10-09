deddd4256e0bbccf79f15c17e6effb29d60f1965
asger@letterbenders.com <asger@letterbenders.com>
2014-10-09 18:29:31 +0200

    added halfs blocks

--- modules/content-blocks.php
+++ modules/content-blocks.php
@@ -14,16 +14,20 @@
  	<div class="container">
  		<div class="row">
 			<div class="col-sm-6 col-lg-6 two-col-item">
-				<div class="item__icon">
-					<img src="http://placehold.it/300x300">
+				<div class="two-col__item">
+					<div class="item__icon">
+						<img src="http://placehold.it/300x300">
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
 				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
 			</div>
 			<div class="col-sm-6 col-lg-6 two-col-item">
-				<div class="item__icon">
-					<img src="http://placehold.it/300x300">
+				<div class="two-col__item">
+					<div class="item__icon">
+						<img src="http://placehold.it/300x300">
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
 				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
 			</div>
  		</div>
  	</div>
@@ -33,10 +37,16 @@
  	<div class="container">
  		<div class="row">
 			<div class="col-sm-6 col-lg-6 two-col-item">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
+				<div class="two-col__item">
+					<h4 class="block-header">Content block header</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
+				</div>
 			</div>
 			<div class="col-sm-6 col-lg-6 two-col-item">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
+				<div class="two-col__item">
+					<h4 class="block-header">Content block header</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam vero sint corporis, ipsum magni et corrupti, natus itaque non eligendi sed dolor. At accusamus ullam cum atque quaerat inventore et?</p>
+				</div>
 			</div>
  		</div>
  	</div>
@@ -45,23 +55,35 @@
 <section class="three-col">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-4 col-xs-12 three-col-item">
-				<div class="item__icon">
-					<img src="http://placehold.it/200x200">
+			<div class="col-sm-4 col-xs-12">
+				<div class="three-col__item">
+					<div class="item__icon">
+						<img src="http://placehold.it/200x200">
+					</div>
+					<h4 class="block-header">Content block header</h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
 				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam repellendus blanditiis, natus, libero quasi explicabo iusto unde ab quidem cum, magni labore praesentium officia, quo veniam placeat eum nulla tenetur!</p>
 			</div>
-			<div class="col-sm-4 col-xs-12 three-col-item">
-				<div class="item__icon">
-					<img src="http://placehold.it/200x200">
+			<div class="col-sm-4 col-xs-12">
+				<div class="three-col__item">
+					<div class="item__icon">
+						<img src="http://placehold.it/200x200">
+					</div>
+					<h4 class="block-header">Content block header</h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
 				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci doloremque non modi, corporis hic nam deleniti itaque pariatur! Sint reprehenderit delectus praesentium quo repudiandae ducimus a eaque sunt et recusandae?</p>
 			</div>
-			<div class="col-sm-4 col-xs-12 three-col-item">
-				<div class="item__icon">
-					<img src="http://placehold.it/200x200">
+			<div class="col-sm-4 col-xs-12">
+				<div class="three-col__item">
+					<div class="item__icon">
+						<img src="http://placehold.it/200x200">
+					</div>
+					<h4 class="block-header">Content block header</h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
 				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
 			</div>
 		</div>
 	</div>
@@ -70,53 +92,120 @@
 <section class="three-col">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-4 col-xs-12 three-col-item">
-				<h4 class="block-header">Content block header</h4>
-				<h5 class="block-subheader">block-Subheader</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam repellendus blanditiis, natus, libero quasi explicabo iusto unde ab quidem cum, magni labore praesentium officia, quo veniam placeat eum nulla tenetur!</p>
+			<div class="col-sm-4 col-xs-12">
+				<div class="three-col__item">
+					<h4 class="block-header">Content block header</h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
+				</div>
 			</div>
-			<div class="col-sm-4 col-xs-12 three-col-item">
-				<h4 class="block-header">Content block header</h4>
-				<h5 class="block-subheader">block-Subheader</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci doloremque non modi, corporis hic nam deleniti itaque pariatur! Sint reprehenderit delectus praesentium quo repudiandae ducimus a eaque sunt et recusandae?</p>
+			<div class="col-sm-4 col-xs-12">
+				<div class="three-col__item">
+					<h4 class="block-header">Content block header</h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
+				</div>
 			</div>
-			<div class="col-sm-4 col-xs-12 three-col-item">
-				<h4 class="block-header">Content block header</h4>
-				<h5 class="block-subheader">block-Subheader</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
+			<div class="col-sm-4 col-xs-12">
+				<div class="three-col__item">
+					<h4 class="block-header">Content block header</h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est amet voluptatem minus natus, dolor consectetur maxime eos incidunt debitis quia mollitia culpa, voluptates laudantium excepturi expedita nihil ullam aliquam, adipisci!</p>
+				</div>
 			</div>
 		</div>
 	</div>
 </section>
 
-<section class="half">
+<section class="two-col">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-6 half__item half__item--text">
-				<h4 class="block-header">Content block header</h4>
-				<h5 class="block-subheader">block-Subheader</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur assumenda facere quibusdam quidem reprehenderit commodi, esse voluptatum repudiandae ratione aut cum laborum consequatur architecto! Quidem ad corporis voluptatem incidunt recusandae ipsa ipsum maxime eligendi, repudiandae, at dicta beatae id nam pariatur iure modi fugit eos ducimus consequuntur! Odio culpa minima quasi! Accusantium officiis dolorem perspiciatis dolores obcaecati veritatis, facilis autem molestias, magni porro nisi quo libero voluptatum blanditiis atque distinctio illum molestiae ratione non voluptatem inventore explicabo hic ipsa. Eius temporibus, labore id! Tempora reiciendis earum natus ut illo sunt omnis nam maxime minus reprehenderit, rerum. Sed pariatur, voluptas incidunt.</p>
-			</div>
-			<div class="col-sm-6 half__item half__item--img">
-				<div class="placeholder">
+			<div class="col-sm-6 col-md-push-6">
+				<div class="two-col__item two-col__item--img">
 					<img src="http://placehold.it/800x400" alt="">
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-pull-6">
+				<div class="two-col__item two-col__item--text">
+					<h4 class="block-header">On small devices im in the bottom </h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur assumenda facere quibusdam quidem reprehenderit commodi, esse voluptatum repudiandae ratione aut cum laborum consequatur architecto! Quidem ad corporis voluptatem incidunt recusandae ipsa ipsum maxime eligendi, repudiandae, at dicta beatae id nam pariatur iure modi fugit eos ducimus consequuntur! Odio culpa minima quasi! Accusantium officiis dolorem perspiciatis dolores obcaecati veritatis, facilis autem molestias, magni porro nisi quo libero voluptatum blanditiis atque distinctio illum molestiae ratione non voluptatem inventore explicabo hic ipsa. Eius temporibus, labore id! Tempora reiciendis earum natus ut illo sunt omnis nam maxime minus reprehenderit, rerum. Sed pariatur, voluptas incidunt.</p>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-<section class="half">
+
+<section class="two-col">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-6 half__item half__item--img">
-				<div class="placeholder">
+			<div class="col-sm-6">
+				<div class="two-col__item two-col__item--img">
 					<img src="http://placehold.it/800x400" alt="">
 				</div>
 			</div>
-			<div class="col-sm-6 half__item half__item--text">
-				<h4 class="block-header">Content block header</h4>
-				<h5 class="block-subheader">block-Subheader</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur assumenda facere quibusdam quidem reprehenderit commodi, esse voluptatum repudiandae ratione aut cum laborum consequatur architecto! Quidem ad corporis voluptatem incidunt recusandae ipsa ipsum maxime eligendi, repudiandae, at dicta beatae id nam pariatur iure modi fugit eos ducimus consequuntur! Odio culpa minima quasi! Accusantium officiis dolorem perspiciatis dolores obcaecati veritatis, facilis autem molestias, magni porro nisi quo libero voluptatum blanditiis atque distinctio illum molestiae ratione non voluptatem inventore explicabo hic ipsa. Eius temporibus, labore id! Tempora reiciendis earum natus ut illo sunt omnis nam maxime minus reprehenderit, rerum. Sed pariatur, voluptas incidunt.</p>
+			<div class="col-sm-6">
+				<div class="two-col__item two-col__item--text">
+					<h4 class="block-header">Content block header</h4>
+					<h5 class="block-subheader">block-Subheader</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur assumenda facere quibusdam quidem reprehenderit commodi, esse voluptatum repudiandae ratione aut cum laborum consequatur architecto! Quidem ad corporis voluptatem incidunt recusandae ipsa ipsum maxime eligendi, repudiandae, at dicta beatae id nam pariatur iure modi fugit eos ducimus consequuntur! Odio culpa minima quasi! Accusantium officiis dolorem perspiciatis dolores obcaecati veritatis, facilis autem molestias, magni porro nisi quo libero voluptatum blanditiis atque distinctio illum molestiae ratione non voluptatem inventore explicabo hic ipsa. Eius temporibus, labore id! Tempora reiciendis earum natus ut illo sunt omnis nam maxime minus reprehenderit, rerum. Sed pariatur, voluptas incidunt.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="halfs">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-6 no-padding halfs__col--1-2">
+				<div class="halfs__item halfs__item">
+					<h4 class="block-header">Halfs block</h4>
+					<h5 class="block-subheader">Can be text or images</h5>	
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio molestiae nihil in eius accusantium quisquam, provident inventore! Autem quo sapiente nam, fugiat adipisci mollitia sit natus amet assumenda provident veniam voluptatem modi tempora magni facere facilis earum quis hic maxime quos quisquam repellendus, suscipit soluta consequuntur sint. Natus quia expedita doloremque nisi blanditiis ut dolore consequatur ipsam tenetur perferendis, nam soluta cupiditate aliquam vel. Impedit aut rerum illo unde consectetur, doloribus perferendis ipsum eum distinctio, eos, ad ullam nulla dignissimos expedita officia. Dolorum incidunt at officia quis. Culpa, sequi! Ipsa laborum nemo maiores totam assumenda esse soluta eius, blanditiis voluptate.</p>				
+				</div>
+			</div>
+			<div class="col-sm-6 no-padding halfs__col--2-2" style="background:url('http://placehold.it/800x400') no-repeat center center; background-size:cover;">
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="halfs">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-6 no-padding halfs__col--1-2" style="background:url('http://placehold.it/800x400') no-repeat center center; background-size:cover;">
+
+			</div>
+			<div class="col-sm-6 no-padding halfs__col--2-2">
+				<div class="halfs__item halfs__item">
+					<h4 class="block-header">Halfs block</h4>
+					<h5 class="block-subheader">Can be text or images</h5>	
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio molestiae nihil in eius accusantium quisquam, provident inventore! Autem quo sapiente nam, fugiat adipisci mollitia sit natus amet assumenda provident veniam voluptatem modi tempora magni facere facilis earum quis hic maxime quos quisquam repellendus, suscipit soluta consequuntur sint. Natus quia expedita doloremque nisi blanditiis ut dolore consequatur ipsam tenetur perferendis, nam soluta cupiditate aliquam vel. Impedit aut rerum illo unde consectetur, doloribus perferendis ipsum eum distinctio, eos, ad ullam nulla dignissimos expedita officia. Dolorum incidunt at officia quis. Culpa, sequi! Ipsa laborum nemo maiores totam assumenda esse soluta eius, blanditiis voluptate.</p>				
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="halfs halfs--red">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-6 no-padding halfs__col--1-2">
+				<div class="halfs__item halfs__item">
+					<h4 class="block-header">Halfs block</h4>
+					<h5 class="block-subheader">Can be text or images</h5>	
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio molestiae nihil in eius accusantium quisquam, provident inventore! Autem quo sapiente nam, fugiat adipisci mollitia sit natus amet assumenda provident veniam voluptatem modi tempora magni facere facilis earum quis hic maxime quos quisquam repellendus, suscipit soluta consequuntur sint. Natus quia expedita doloremque nisi blanditiis ut dolore consequatur ipsam tenetur perferendis, nam soluta cupiditate aliquam vel. Impedit aut rerum illo unde consectetur, doloribus perferendis ipsum eum distinctio, eos, ad ullam nulla dignissimos expedita officia. Dolorum incidunt at officia quis. Culpa, sequi! Ipsa laborum nemo maiores totam assumenda esse soluta eius, blanditiis voluptate.</p>				
+				</div>
+			</div>
+			<div class="col-sm-6 no-padding halfs__col--2-2">
+				<div class="halfs__item halfs__item">
+					<h4 class="block-header">Halfs block</h4>
+					<h5 class="block-subheader">Can be text or images</h5>	
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio molestiae nihil in eius accusantium quisquam, provident inventore! Autem quo sapiente nam, fugiat adipisci mollitia sit natus amet assumenda provident veniam voluptatem modi tempora magni facere facilis earum quis hic maxime quos quisquam repellendus, suscipit soluta consequuntur sint. Natus quia expedita doloremque nisi blanditiis ut dolore consequatur ipsam tenetur perferendis, nam soluta cupiditate aliquam vel. Impedit aut rerum illo unde consectetur, doloribus perferendis ipsum eum distinctio, eos, ad ullam nulla dignissimos expedita officia. Dolorum incidunt at officia quis. Culpa, sequi! Ipsa laborum nemo maiores totam assumenda esse soluta eius, blanditiis voluptate.</p>				
+				</div>
 			</div>
 		</div>
 	</div>
